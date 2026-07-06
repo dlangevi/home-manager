b3916661bf56a1ab7f83538351ba30f817cf43f3
David Langevin <dlangevi@uwaterloo.ca>
2026-07-06 14:10:15 -0700

    feat(nixos): migrate dance configuration into flake

--- nixos/hosts/dance.nix
+++ nixos/hosts/dance.nix
@@ -1,1 +1,28 @@
-{ ... }: { networking.hostName = "dance"; system.stateVersion = "24.11"; }
+{ pkgs, ... }:
+
+{
+  networking.hostName = "dance";
+  system.stateVersion = "24.11";
+
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.displayManager.defaultSession = "plasma";
+  services.xserver.displayManager.sddm.wayland.enable = true;
+
+  services.xserver.displayManager.autoLogin.enable = true;
+  services.xserver.displayManager.autoLogin.user = "dance";
+
+  services.udev.extraRules = ''
+      SUBSYSTEMS=="hidraw", ACTION=="add", MODE="0660", GROUP="dance"
+  '';
+
+  users.groups.dance = { };
+  users.users.dance = {
+    isNormalUser = true;
+    shell = pkgs.zsh;
+    description = "dance";
+    extraGroups = [ "networkmanager" "wheel" "dance" ];
+    packages = [ ];
+  };
+
+  environment.systemPackages = with pkgs; [ unzip ];
+}
