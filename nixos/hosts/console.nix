73118e016fbb05d0d7d5e49f7bc2bb18465275d6
David Langevin <dlangevi@uwaterloo.ca>
2026-07-06 14:08:20 -0700

    feat(nixos): migrate console configuration into flake

--- nixos/hosts/console.nix
+++ nixos/hosts/console.nix
@@ -1,1 +1,71 @@
-{ ... }: { networking.hostName = "console"; system.stateVersion = "23.11"; }
+{ config, pkgs, ... }:
+
+{
+  networking.hostName = "console";
+  system.stateVersion = "23.11";
+
+  # Extended Bluetooth policy for HID console use
+  hardware.bluetooth.settings = {
+    General = {
+      Experimental = true;
+      FastConnectable = false;
+    };
+    Policy = {
+      AutoEnable = false;
+    };
+  };
+
+  # Display manager
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.displayManager.defaultSession = "plasma";
+  services.xserver.displayManager.sddm.wayland.enable = true;
+
+  # Auto-login as console
+  services.xserver.displayManager.autoLogin.enable = true;
+  services.xserver.displayManager.autoLogin.user = "console";
+
+  # udev hidraw rule
+  services.udev.extraRules = ''
+      SUBSYSTEMS=="hidraw", ACTION=="add", MODE="0660", GROUP="console"
+  '';
+
+  # User
+  users.groups.console = { };
+  users.users.console = {
+    isNormalUser = true;
+    shell = pkgs.zsh;
+    description = "console";
+    extraGroups = [ "networkmanager" "wheel" "console" ];
+    packages = with pkgs; [ kdePackages.kate ];
+  };
+
+  # Steam extras
+  programs.steam.protontricks.enable = true;
+  programs.steam.extraCompatPackages = [ pkgs.proton-ge-bin ];
+
+  # CPU freq cap for Ryzen 5 3550H thermal
+  powerManagement.cpuFreqGovernor = "schedutil";
+  powerManagement.resumeCommands = ''
+    ${config.boot.kernelPackages.cpupower}/bin/cpupower frequency-set -u 2100MHz
+  '';
+  systemd.services.cap-cpu-freq = {
+    description = "Cap CPU max frequency (disable turbo on Ryzen 5 3550H)";
+    wantedBy = [ "multi-user.target" ];
+    after = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStart = "${config.boot.kernelPackages.cpupower}/bin/cpupower frequency-set -u 2100MHz";
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    cmake
+    gnumake
+    nodejs_22
+    python3
+    gparted
+    noto-fonts-cjk-sans
+    config.boot.kernelPackages.cpupower
+  ];
+}
